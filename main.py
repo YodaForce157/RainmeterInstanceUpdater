4cd743332941761bd2dc33a6811ef86ff0450850
YodaForce157 <69751882+YodaForce157@users.noreply.github.com>
2024-04-16 22:42:41 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+import configparser
+
+folder = 'instances folder'
+instances = [name for name in os.listdir(folder) if os.path.isdir(os.path.join(folder, name))]
+blacklist = ['.LAUNCHER_TEMP', '.tmp']
+blacklistSet = set(blacklist)
+configFile = 'YourConfig.iniFilePath'
+i=0
+
+def DeleteInstanceSections(configFile):
+    curr_config = configparser.ConfigParser(strict=False)
+    curr_config.read(configFile, encoding="utf-16")
+    for sections in curr_config.sections():
+        if sections.startswith("INSTANCE-") == True:
+            curr_config.remove_section(sections)
+    with open('config file', "w", encoding="utf-16") as fp:
+        curr_config.write(fp)
+        fp.close()
+
+def CreateInstanceImage(instanceN):
+    write_config = configparser.ConfigParser()
+    sectionName = f"INSTANCE-{instanceN}"
+    write_config.add_section(sectionName)
+
+    return write_config
+
+DeleteInstanceSections()
+
+for instance in instances:
+    instanceInBlacklist = instance in blacklistSet
+    if not instanceInBlacklist:
+        with open(configFile, "a", encoding="utf-16") as fp:
+            CreateInstanceImage(instance).write(fp)
+            i = i + 1
+            fp.close()
